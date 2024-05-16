33ba922937bd2a3b5e9520b11c4bb6934e988fa5
Anup DSouza <anup.dsouza@gmail.com>
2024-05-16 10:29:28 +0530

    Add initial files
    Add firebase packages & initialisation code

---
+++ VertexFirebaseSwiftUI/VertexFirebaseSwiftUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  VertexFirebaseSwiftUI
+//
+//  Created by Anup D'Souza on 16/05/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VertexFirebaseSwiftUI/VertexFirebaseSwiftUI/VertexFirebaseSwiftUIApp.swift
@@ -0,0 +1,34 @@
+//
+//  VertexFirebaseSwiftUIApp.swift
+//  VertexFirebaseSwiftUI
+//
+//  Created by Anup D'Souza on 16/05/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct VertexFirebaseSwiftUIApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
